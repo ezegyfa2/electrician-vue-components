8d7c6f534c6d26361e54cb768ba8f19ce32819e3
József Bagota <jozsef.bagota@gmail.com>
2023-09-06 19:55:22 +0200

    making responsive the footer and the description parts of the site

--- src/Templates/templateRegister.js
+++ src/Templates/templateRegister.js
@@ -4,7 +4,7 @@ export default {
             window.templates = {}
         }
         window.templates.electrician_products = require('./products').default
-        window.templates.electrician_index = require('./index').default
+        window.templates.electrician_welcome = require('./welcome').default
 
         window.templates.electrician_layout = require('./Layout/layout').default
         window.templates.electrician_navigation_bar = require('./Layout/navigationBar').default
